02dac64658ae600e8a2c422e4f438cd1b4e7cff4
Mohamed <takarage1997@gmail.com>
2019-04-27 17:00:24 +0100

    initial CETD algorithm implementation

---
+++ features_extraction/cetd.py
@@ -0,0 +1,74 @@
+import sys
+import os
+
+sys.path.append(os.path.realpath(os.path.abspath('../utilities')))
+
+
+from dom_mapper import DOM_Mapper
+
+
+
+
+class CETD(DOM_Mapper):
+    
+    
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    
+    
+    def count_tags(self, node):
+        
+        node['tagsCount'] = len(node['children'])
+        
+        for child in node['children']:
+            self.count_tags(child)
+            node['tagsCount'] += child['tagsCount']
+            
+        
+        pass
+    
+    
+    def calculate_text_density(self, node):
+        
+        node['textDensity'] = len(node['text'])/(node['tagsCount'] if node['tagsCount'] > 0 else 1)  
+        return node
+        
+        pass
+    
+    
+    def calculate_density_sum(self, node):
+          
+        node['densitySum'] = 0
+        for child in node['children']:            
+            self.calculate_density_sum(child)
+            node['densitySum'] += child['textDensity']
+            
+            
+        
+        pass
+    
+    
+    def test(self, arg):
+        print("from child {}".format(arg))
+        pass
+    
+    
+    pass
+
+
+if __name__ == '__main__':
+    
+    #cetd = CETD()
+    #cetd.retrieve_DOM_tree(os.path.realpath('../datasets/extracted_data/0000.json'))
+    #cetd.count_tags(cetd.DOM)
+    
+    #cetd.map(node = cetd.DOM, fun1 = cetd.calculate_text_density)
+    #cetd.update_DOM_tree()
+    #cetd.calculate_density_sum(cetd.DOM)
+    
+    pass
+
+
+
--- utilities/dom_mapper.py
+++ utilities/dom_mapper.py
@@ -12,29 +12,35 @@ class DOM_Mapper:
     
     
     def __init__(self):
-        
+        # ...
         pass
     
     
-    def retrieve_dom_tree(self, file_path):
+    def retrieve_DOM_tree(self, file_path):
     
+        self.DOM_file_path = os.path.realpath(file_path)
+        
         file = open(file_path, 'r', encoding = 'UTF-8')
         page_data = json.load(file)
         self.DOM = page_data['DOM']
         self.meta_data = page_data['meta_data']
         
-        print(self.meta_data)
-        
+        file.close()
         pass
     
     
     
-    def map(self, node, fun, option = 'DEPTH'):
+    def map(self, node = None, 
+            fun1 = (lambda x: x), 
+            fun2 = (lambda x, y: (x,y)), 
+            fun3 = (lambda x, y: (x,y)), 
+            fun4 = (lambda x: x), 
+            option = 'DEPTH'):
         
         if option == 'DEPTH':
-            self.depth_mapper(node, fun)
+            self.depth_mapper(node, fun1, fun2, fun3, fun4)
         elif option == 'BREADTH':
-            self.breadth_mapper(node, fun)
+            self.breadth_mapper(node, fun1, fun2, fun3, fun4)
         else:
             print('ERR: option "%(option)s" is not available!' % {'option':option})
         
@@ -42,37 +48,68 @@ class DOM_Mapper:
     
     
     
-    def depth_mapper(self, node, fun):
+    def depth_mapper(self, node, fun1, fun2, fun3, fun4):
         
-        node = fun(node)
+        node = fun1(node)
         
         for child in node['children']:
-            self.map(node = child, fun = fun)
+            node, child = fun2(node, child)
+            self.depth_mapper(child, fun1, fun2, fun3, fun4)
+            node, child = fun3(node, child)
+            
+        node = fun4(node)
         
         pass
     
     
     
     
-    def breadth_mapper(self, node, fun):
+    def breadth_mapper(self, node, fun1, fun2, fun3, fun4):
         # ...
         pass
     
     
     
+    def update_DOM_tree(self):
+        
+        file = open(self.DOM_file_path, 'w', encoding = 'UTF-8')
+        
+        DOM_str = json.dumps({
+                'DOM':self.DOM,
+                'meta_data':self.meta_data
+                })
+        
+        file.write(DOM_str)
+        
+        file.close()
+        
+        pass
+    
+    
+    def test(self, arg):
+        print("from parent : {}".format(arg))
+        
+    
+        
+        pass
+    
     
     pass
 
 
+def p(node):
+    
+    print(node['tagName'])
+    return node
 
 
 if __name__ == '__main__':
     
     dr = DOM_Mapper()
 
-    dr.retrieve_dom_tree('../datasets/extracted_data/0000.json')
+    dr.retrieve_DOM_tree('../datasets/extracted_data/0000.json')
     
-    #dr.map(node = dr.DOM, fun = ) 
+    dr.map(node = dr.DOM, fun1 = p) 
     #dr.map(node = dr.DOM, fun = )
     
     
